8ecdab251010ae3635d69e5e3be3ac518a33825d
AlexBouillaguet <alexandre.bouillaguet>
2024-11-18 13:11:37 +0100

    add Footer

--- Argent Bank/src/pages/Home/index.jsx
+++ Argent Bank/src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import Hero from '../../components/Hero';
 import Features from '../../containers/Features';
@@ -10,6 +11,7 @@ const Home = () => {
           <Hero />
           <Features />
          </main>
+         <Footer />
        </>
      );
    }
