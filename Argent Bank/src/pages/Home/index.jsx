e9dcf1d04d0e0f4c8888e11720f0c957ec8f197a
AlexBouillaguet <alexandre.bouillaguet>
2024-11-18 12:57:57 +0100

    add Features

--- Argent Bank/src/pages/Home/index.jsx
+++ Argent Bank/src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import Header from '../../components/Header';
 import Hero from '../../components/Hero';
+import Features from '../../containers/Features';
 
 const Home = () => {
      return (
@@ -7,6 +8,7 @@ const Home = () => {
          <Header />
          <main>
           <Hero />
+          <Features />
          </main>
        </>
      );
