3622d5aeac75dfef94e8e1269d933c2cfc309ee4
AlexBouillaguet <alexandre.bouillaguet>
2024-11-18 12:30:25 +0100

    add Hero

--- Argent Bank/src/pages/Home/index.jsx
+++ Argent Bank/src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
 import Header from '../../components/Header';
+import Hero from '../../components/Hero';
 
 const Home = () => {
      return (
        <>
          <Header />
          <main>
-          
+          <Hero />
          </main>
        </>
      );
