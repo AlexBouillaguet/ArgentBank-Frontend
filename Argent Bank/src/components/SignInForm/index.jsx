493c35ae3c173f9c6f4a122804b6c5a15ef3349f
AlexBouillaguet <alexandre.bouillaguet>
2024-11-21 10:34:59 +0100

    add redux store / userSlice

--- Argent Bank/src/components/SignInForm/index.jsx
+++ Argent Bank/src/components/SignInForm/index.jsx
@@ -1,27 +1,80 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  loginStart,
+  loginSuccess,
+  loginFailure,
+} from "../../features/userSlice";
 import "./index.scss";
 
-const SignIn = () => {
-     return (
-       <section className="sign-in-content">
-         <i className="fa fa-user-circle sign-in-icon"></i>
-         <h1>Sign In</h1>
-         <form>
-           <div className="input-wrapper">
-             <label htmlFor="username">Username</label>
-             <input type="text" id="username" />
-           </div>
-           <div className="input-wrapper">
-             <label htmlFor="password">Password</label>
-             <input type="password" id="password" />
-           </div>
-           <div className="input-remember">
-             <input type="checkbox" id="remember-me" />
-             <label htmlFor="remember-me">Remember me</label>
-           </div>
-           <a href="./user.html" className="sign-in-button">Sign In</a>
-         </form>
-       </section>
-     );
-   };
-   
-   export default SignIn;
+const SignInForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isLoading, error } = useSelector((state) => state.user);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(loginStart());
+
+    try {
+      const response = await fetch("http://localhost:3001/api/v1/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Login failed");
+      }
+
+      const data = await response.json();
+      dispatch(loginSuccess(data.user));
+      localStorage.setItem("token", data.token);
+
+      window.location.href = `${window.location.origin}/user`;
+    } catch (err) {
+      dispatch(loginFailure(err.message));
+    }
+  };
+
+  return (
+    <section className="sign-in-content">
+      <i className="fa fa-user-circle sign-in-icon"></i>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="input-wrapper">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="input-remember">
+          <input type="checkbox" id="remember-me" />
+          <label htmlFor="remember-me">Remember me</label>
+        </div>
+        <button type="submit" className="sign-in-button" disabled={isLoading}>
+          {isLoading ? "Signing In..." : "Sign In"}
+        </button>
+      </form>
+      {error && <p className="error-message">{error}</p>}
+    </section>
+  );
+};
+
+export default SignInForm;
